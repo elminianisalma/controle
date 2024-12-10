1c18934e7a1a031724c43708a24fbc169bc35e67
elminianisalma <164582550+elminianisalma@users.noreply.github.com>
2024-12-10 03:09:49 +0100

    Add files via upload

---
+++ ecom-application/billing-service/src/main/java/org/sid/billingservice/services/CustomerRestClient.java
@@ -0,0 +1,17 @@
+package org.sid.billingservice.services;
+
+import org.sid.billingservice.model.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "CUSTOMER-SERVICE")
+public interface CustomerRestClient {
+    @GetMapping(path="/customers/{id}")
+    Customer findCustomerById(@PathVariable Long id);
+
+    @GetMapping("/api/customers")
+    PagedModel<Customer> getAllCustomers();
+
+}
